4b24aa4ea004e5ef4397f64f478cf137a708ba12
Mario Durántez <mdurantezs@gmail.com>
2021-11-24 18:19:40 +0100

    [ingest_tweets] Move config to env vars, more info

--- cloud_functions/ingest_tweets/main.py
+++ cloud_functions/ingest_tweets/main.py
@@ -1,4 +1,4 @@
-import yaml
+import os
 
 import twint
 from google.cloud import bigquery
@@ -19,7 +19,7 @@ def get_tweets_data(search_string:str, limit: int):
     return tweets_df
 
 
-def save_df_to_bq(df, schema, table_id:str):
+def save_df_to_bq(df, schema, table_id:str) -> int:
     client = bigquery.Client()
 
     job_config = bigquery.LoadJobConfig(
@@ -33,16 +33,16 @@ def save_df_to_bq(df, schema, table_id:str):
     job.result()
 
     table = client.get_table(table_id)
-    print(
-        "Loaded {} rows and {} columns to {}".format(
-            table.num_rows, len(table.schema), table_id
-        )
-    )
+    return(table.num_rows)
 
 
 def ingest_tweets(request):
-    with open('config.yml', 'r') as file:
-        conf = yaml.safe_load(file)
+    tweet_limit = os.getenv('SCRAP_MAX_TWEETS', 1000)
+    if 'SCRAP_DESTINATION_TABLE' not in os.environ:
+        return "Environment variable not set: 'SCRAP_DESTINATION_TABLE'"
+    else:
+        destination_table = os.environ['SCRAP_DESTINATION_TABLE']
+
 
     if request.args and 'character' in request.args:
         character = request.args.get('character')
@@ -52,7 +52,7 @@ def ingest_tweets(request):
         search_query = request.args.get('search_query')
     else: return "Missing 'search_query' argument"
 
-    tweets_df = get_tweets_data(search_query, conf['tweeet_limit'])
+    tweets_df = get_tweets_data(search_query, tweet_limit, start_date=start_date, end_date=end_date)
 
     tweets_df['created_at'] = tweets_df['created_at'].astype(int)
     tweets_df['hashtags'] = tweets_df['hashtags'].astype(str)
@@ -62,4 +62,6 @@ def ingest_tweets(request):
     tweets_df['reply_to'] = tweets_df['reply_to'].astype(str)
     tweets_df['character'] = character
 
-    save_df_to_bq(tweets_df, BQ_TABLE_SCHEMA, conf['destination_table'])
+    loaded_rows = save_df_to_bq(tweets_df, BQ_TABLE_SCHEMA, destination_table)
+
+    return f'Loaded {loaded_rows} tweets'
